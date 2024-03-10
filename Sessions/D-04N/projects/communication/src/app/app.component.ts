a082fd5034825261b569fd09a2016157077fec49
guyvinay <mrsinghvinay563@gmail.com>
2024-03-10 14:57:19 +0530

    *ngIf-*ngFor-directive

--- Sessions/D-04N/projects/communication/src/app/app.component.ts
+++ Sessions/D-04N/projects/communication/src/app/app.component.ts
@@ -6,30 +6,8 @@ import { ChildComponent } from "./child.component";
     selector: 'app-root',
     standalone: true,
     template: `
-  <h1>From AppComponent</h1>
-  <!-- <app-child quantity="{{count}}" > -->
-  <app-child [quantity]="count" (childEvent)="handleChildEmit($event)" >
-    <h3>Heading 3</h3>
-    <h4>Heading 4</h4>
-    <button>Click</button>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae sapiente molestias culpa dolor atque magnam in autem sint maxime? Beatae.</p>
-    <p class="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae sapiente molestias culpa dolor atque magnam in autem sint maxime? Beatae.</p>
-    <div>
-      {{title}}
-    </div>
-    <div id="title">
-      {{title}}
-    </div>
-    <ul>
-      <li>List Item 1 </li>
-      <li>List Item 2 </li>
-      <li>List Item 3 </li>
-      <li>List Item 4 </li>
-      <li>List Item 5 </li>
-    </ul>
-  </app-child>
-  <button (click)="count=count+1">Count Button : {{count}} </button>
-  <h2>{{message}}</h2>
+    <!-- <h1>Hello WOlf</h1> -->
+    
   `,
     imports: [RouterOutlet, ChildComponent]
 })
--- Sessions/D-04N/projects/directives/src/app/app.component.ts
+++ Sessions/D-04N/projects/directives/src/app/app.component.ts
@@ -1,16 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+  <label for="terms">Show Terms and Conditions</label>
+  <input id="terms" type="checkbox" (change)="show = !show">
+  <!-- <input id="terms" type="checkbox" (click)="show = !show"> -->
+    <fieldset *ngIf="show">
+      <legend>Terms and Conditions</legend>
+      <hr>
+      <p>1st Term </p>
+      <p>2st Term </p>
+      <p>3st Term </p>
+      <p>4st Term </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos labore ullam voluptates totam fugit dolorum vitae aliquam voluptatibus architecto aperiam!</p>
+    </fieldset>
 
-    
+    <div *ngIf="!show" >
+      {{title}}
+    </div>
+
+    <ng-template [ngIf]="show" >
+       ng-template: {{title}}
+    </ng-template>
+
+    <ol>
+      <li *ngFor="let hero of avengers" >{{hero}}</li>
+    </ol>
+
   `,
   styles: [],
 })
 export class AppComponent {
   title = 'directives';
+  show=true;
+avengers=["Ironman", "Captain America", "Thor", "Black Widow", "Hulk", "Nick Fury", "Doctor Strange","Antman", "Iron Machine"];
 }
