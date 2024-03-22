7c56018eb255db24eb6adb2853cb5866a492659a
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2024-03-22 09:12:10 +0000

    CRUD-inits

---
+++ Assignments/Ng-CRUD/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { DisplayComponent } from './display/display.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormComponent, DisplayComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Ng-CRUD';
+}
