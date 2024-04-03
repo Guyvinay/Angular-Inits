009f83677f7b7647331a03db3d27246b51b62b05
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2024-04-03 07:36:40 +0000

    Housing-Service-implemented

--- Angular/Ng-Homes/src/app/home/home.component.ts
+++ Angular/Ng-Homes/src/app/home/home.component.ts
@@ -1,27 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { Housinglocation } from '../housinglocation';
+import { CommonModule } from '@angular/common';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HousingLocationComponent],
+  imports: [HousingLocationComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  housingService:HousingService = inject(HousingService);
 
-  housingLocation:Housinglocation={
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
+  constructor(
+    // private housingService : HousingService,
+  ){}
+
+  ngOnInit(): void {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
   }
+
+
+  // housingLocation:Housinglocation={
+  //   id: 9999,
+  //   name: 'Test Home',
+  //   city: 'Test city',
+  //   state: 'ST',
+  //   photo: `${this.baseUrl}/example-house.jpg`,
+  //   availableUnits: 99,
+  //   wifi: true,
+  //   laundry: false,
+  // }
+
+  housingLocationList: Housinglocation[] = [];
 
 }
